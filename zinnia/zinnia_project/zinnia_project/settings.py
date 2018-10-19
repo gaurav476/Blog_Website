dd12af5639bfed0d1fc94e2b18e4e5b791b1f714
nandini <nandini.soni8@gmail.com>
2018-10-20 02:58:07 +0530

    Text to speech

--- zinnia/zinnia_project/zinnia_project/settings.py
+++ zinnia/zinnia_project/zinnia_project/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'zinnia.context_processors.version',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -124,5 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [STATIC_ROOT,]
 
+MEDIA_ROOT = ''
+MEDIA_URL = '/uploads/'
 SITE_ID = 1
--- zinnia/zinnia_project/zinnia_project/urls.py
+++ zinnia/zinnia_project/zinnia_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^weblog/', include('zinnia.urls')),
+    #url(r'weblog/', include('zinnia.urls')), original
+    url('', include('zinnia.urls')),
     url(r'^comments/', include('django_comments.urls')),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
